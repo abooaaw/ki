20432aab235d4bac38a4707353b637d9e01df071
abooaaw <104279173+abooaaw@users.noreply.github.com>
2023-01-15 13:39:21 +0300

    Add files via upload

---
+++ project try n1/script.js
@@ -0,0 +1,25 @@
+<><script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script></>
+$(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+    $(".side-nav .collapse").on("hide.bs.collapse", function() {                   
+        $(this).prev().find(".fa").eq(1).removeClass("fa-angle-right").addClass("fa-angle-down");
+    });
+    $('.side-nav .collapse').on("show.bs.collapse", function() {                        
+        $(this).prev().find(".fa").eq(1).removeClass("fa-angle-down").addClass("fa-angle-right");        
+    });
+})    
+function logIn() {
+    // get user input
+    var user = document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
+  
+    // check if username and password are correct
+    if (user === "1" && pass === "1") {
+      window.location.href = "http://127.0.0.1:5500/asm.html";
+    } else if (user === "worker" && pass === "password") {
+      // go to worker mode page
+      window.location.href = "http://127.0.0.1:5500/asm.html";
+    } else {
+      alert("Incorrect username or password. Please try again.");
+    }
+  }
